bd19ff43335beade01a52f5571ff51a873a7c99c
Jose Carlos Sáez Fernández <jcsaez.dev@gmail.com>
2024-09-28 14:26:38 +0200

    catch error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const app = require("./app")
 
 const main = async () => {
-    app.listen(8080)
-    console.log("Server is running on port 3000");
+    try {
+       app.listen(8080)
+    console.log("Server is running on port 3000"); 
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 main()
