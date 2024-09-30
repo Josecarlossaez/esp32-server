481dd57b8e808c1813016b983177b669064e61c7
Jose Carlos Sáez Fernández <jcsaez.dev@gmail.com>
2024-09-30 14:05:47 +0200

    ws installed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const WebSocket = require('ws');
+
+// Crear servidor WebSocket
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+    
+    // Escuchar mensajes del cliente
+    ws.on('message', (message) => {
+        console.log('Mensaje recibido del cliente:', message);
+        
+        // Enviar respuesta al cliente
+        ws.send('Mensaje recibido: ' + message);
+    });
+
+    ws.on('close', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
 router.get('/', (req, res) => {
 
     res.json({message:"Hello World"})
